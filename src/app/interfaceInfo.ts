7ff4e706c3427e63733e20d2452ff05c51d35939
milan.zivanov <milan.zivanov@gmail.com>
2018-05-15 15:06:56 +0200

    new main bg img interface info

--- src/app/interfaceInfo.ts
+++ src/app/interfaceInfo.ts
@@ -2,30 +2,8 @@ export interface RootObject {
   id: number;
   name: string;
   username: string;
-  email: string;
-  address: Address;
-  phone: string;
-  website: string;
-  company: Company;
   routerLink: string;
+  icon: string;
   expi: string;
-}
-
-interface Company {
-  name: string;
-  catchPhrase: string;
-  bs: string;
-}
-
-interface Address {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: Geo;
-}
-
-interface Geo {
-  lat: string;
-  lng: string;
+  website?: string;
 }
