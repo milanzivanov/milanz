d9a0e3867b039726e0a0090911a39ab389deea96
milan.zivanov <milan.zivanov@gmail.com>
2018-08-28 11:00:45 +0200

    responsive for mobile devices and new img slider design

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  url: '../assets/db.json'
+  url: 'https://next.json-generator.com/api/json/get/4yOCCLqIB'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  url: '../assets/db.json'
+  url: 'https://next.json-generator.com/api/json/get/4yOCCLqIB'
 };
